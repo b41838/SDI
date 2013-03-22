3f98d16f876336a7e42fd92f51db33c22949592e
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-03-21 19:05:52 -0600

    first two functions complete.

--- project3/schroeder_kyler_project3.js
+++ project3/schroeder_kyler_project3.js
@@ -1,1 +1,46 @@
-alert("JavaScript works!");
+/* 
+ * 
+ * Author: Kyler Schroeder
+ * Created for: SDI Online
+ * Week Three project, Tardaaaaaar!.
+ * 
+ */
+
+
+var grumpyCat = {
+	name: "Tardar Sauce",
+	grumpy: true,
+	hatedThings: 12,
+	iHate: [ "spring", "outdoors", "christmas", "flowers", "babies", "people", "barney", "smiles", "internet explorer", "cuddly things", "friskeys", "me", "you"],
+	brother: [ pokey, "Pokey" ]
+}
+
+var pokey = {
+	relationship: "little brother"
+}
+
+function func1(x, y) {
+	return console.log("I " + x + " at sxsw, it was " + y) + ".";
+}
+
+function func2(love, hate) {
+	console.log("Someone asked me if I hate my little brother.  Do i?")
+	for(var i = 0; i < love.iHate.length; i++) {
+		if(grumpyCat.iHate[i] = hate) {
+			console.log("Yes, is that mean?  GOOD.")
+			return;
+		}
+		else {
+			console.log("I don\'t hate Pokey, just you.")
+			return;
+		}
+		
+	}
+}
+
+
+
+
+
+var1 = func1("took pictures", "aweful");
+func2(grumpyCat, grumpyCat.brother[0]);
