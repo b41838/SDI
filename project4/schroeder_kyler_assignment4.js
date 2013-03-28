125edffffd042ed5fc7ffd6e84561a190c374b91
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-03-28 13:17:28 -0600

    Added place holders for future functions and array function that sorts by key.

--- project4/schroeder_kyler_assignment4.js
+++ project4/schroeder_kyler_assignment4.js
@@ -7,10 +7,10 @@
  */
 
 
-// validate phone number
-function validNumber() {
-	var num = '123-456-7890';
-    var chopIt = num.split('-'); // this is an array containing the items
+// Does a string follow a 123-456-7890 pattern like a phone number?
+function validNumber(gimme) {
+	// var gimme = 0;
+    var chopIt = gimme.split('-'); // this is an array containing the items
 
 	for (var i = 0; i < chopIt.length; i++) {
 		if(isNaN(chopIt[i])) {
@@ -21,15 +21,122 @@ function validNumber() {
     	}
 	}
 
-	var end1 = num.indexOf("-");
-	var start2 = num.indexOf("-")+1;
-	var end2 = num.lastIndexOf("-");
-	var start3 = num.lastIndexOf("-")+1
-	var end3 = num.length;
+	var end1 = gimme.indexOf("-");
+	var start2 = gimme.indexOf("-")+1;
+	var end2 = gimme.lastIndexOf("-");
+	var start3 = gimme.lastIndexOf("-")+1
+	var end3 = gimme.length;
 	
-	console.log(num.substring(0, end1) + num.substring(start2, end2) + num.substring(start3, end3));
+	console.log(gimme.substring(0, end1) + gimme.substring(start2, end2) + gimme.substring(start3, end3));
+
+	/*var whereDash = gimme.length;
+	for (var ni = 0; ni <= whereDash; ni++) {
+		if (gimme.substring(ni, ni+1) === "-") {
+			// return ni+1;
+			console.log("1");
+			for (var j = ni; j <= whereDash; j++) {
+				if gimme.substring(j, j+1) === "-")
+				console.log("2");
+			}
+		}
+	}*/
 
 }
 
-validNumber();
+// Does a string follow an aaa@bbb.ccc pattern like an email address?
+function emailVal() {
+	
+	
+	
+};
 
+
+// Is the string a URL? (Does it start with http: or https:?)
+function isURL() {
+	
+	
+	
+};
+
+
+// Title-case a string (split into words, then uppercase the first letter of each word)
+function titleCase() {
+	
+	
+	
+};
+
+
+/* Given a string that is a list of things separated by a given string, as well as another string separator,
+   return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c". */
+function separator() {
+	
+	
+	
+};
+
+
+// Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+function decimalPlaces() {
+	
+	
+	
+};
+
+
+// Fuzzy-match a number: is the number above or below a number within a certain percent?
+function fuzzyMatch() {
+	
+	
+	
+};
+
+
+// Find the number of hours or days difference between two dates.
+function differenceDates() {
+	
+	
+	
+};
+
+
+// Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+function stringVersion() {
+	
+	
+	
+};
+
+
+// Find the smallest value in an array that is greater than a given number
+function biggerThan() {
+
+
+
+}
+
+
+// Find the total value of just the numbers in an array, even if some of the items are not numbers.
+function totalOfNumbers() {
+
+
+
+};
+
+
+/* Given an array of objects and the name of a key, return the array sorted by the value of that key in each
+   of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].*/
+function sortedArray(array, key) {
+	
+	    return (array.sort(function(a,b) {
+	    	return a[key] - b[key];
+	    }));
+};
+
+
+
+// validNumber("123-456-7890");
+
+// Sorts array by key - totally works!
+var nonSortedArray = [{a:1}, {a:5}, {a:3}, {a:2}, {a:4}, {b:1}];
+console.log(sortedArray(nonSortedArray, "a"));
