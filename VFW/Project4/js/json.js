7317022140973cd56d52718f4360d0abc2c529c4
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-04-30 21:44:15 -0600

    Initial commit containing file structure with new image folder and single image as well as a json object filein the js folder.  Project inserts dummy json data if no data is stored in localStorage.

---
+++ VFW/Project4/js/json.js
@@ -0,0 +1,10 @@
+var json = {
+	"dummyData": {
+		"date":     ["Date:", "2012-4-20"],
+		"sugar":    ["Sugar Level:", "High"],
+		"pillName": ["Pill Name:", "Red Pill"],
+		"quantity": ["Pill Quantity:", "1"],
+		//"required": ["Is Required:", requiredPill],
+		"notes":    ["Notes:", "Word."]
+	}
+}
